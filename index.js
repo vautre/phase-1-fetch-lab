b2c478a5c0bc3d36f97b86de712d633270c75998
Joyce Sheng <joycesheng96@hotmail.com>
2024-10-16 11:22:34 -0400

    saving

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books", {
+    headers: {'Accept' : `application.json`}
+  })
+  .then((response) => response.json())
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
